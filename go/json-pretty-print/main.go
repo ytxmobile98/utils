a0b755cfd2e88501a7c173fc549712fe9262eff1
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-05-31 16:54:45 +0800

    feat: json pretty print

--- go/json-pretty-print/main.go
+++ go/json-pretty-print/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/ytxmobile98/utils/go/utils"
 )
+
+type Args struct {
+	inputFilename  string
+	outputFilename string
+
+	indent uint
+}
+
+const defaultIndent uint = 4
+
+var args Args
+
+func init() {
+	parseArgs := func() {
+		flag.StringVar(&args.inputFilename, "i", "", "input json file (required)")
+		flag.StringVar(&args.outputFilename, "o", "", "output json file (optional; if not specified, write to stdout)")
+
+		flag.UintVar(&args.indent, "p", defaultIndent, fmt.Sprintf("number of spaces used for pretty indent, max: %d; default: %d", utils.PrettyPrintMaxIndent, defaultIndent))
+
+		flag.Parse()
+	}
+
+	checkArgs := func(errs *[]error) {
+		if args.inputFilename == "" {
+			*errs = append(*errs, fmt.Errorf("input json file is required"))
+		}
+	}
+
+	utils.ParseFlagsAndCheckErrors(parseArgs, checkArgs, 1)
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -13,11 +44,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	inputFilename := os.Args[1]
-	output, err := utils.PrettyPrintJSONFile(inputFilename, 4)
+	inputFilename := args.inputFilename
+	output, err := utils.PrettyPrintJSONFile(inputFilename, args.indent)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
 	}
-	fmt.Println(string(output))
+	if args.outputFilename != "" {
+		err = os.WriteFile(args.outputFilename, output, 0644)
+	} else {
+		_, err = os.Stdout.Write(output)
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
 }
